cdaa02768880f95f18342671572ea47fd45a675b
Mostafiz1257 <mostafiz1257@gmail.com>
2023-02-18 15:09:07 +0600

    added input validation section

--- genius.js
+++ genius.js
@@ -1,3 +1,4 @@
+// Located page with blog button 
 document.getElementById('blog-btn').addEventListener('click', function () {
     window.location.href = 'question.html';
 })
@@ -5,13 +6,26 @@ document.getElementById('blog-btn').addEventListener('click', function () {
 // First card 
 document.getElementById('trangle-btn').addEventListener('click', function () {
     
+
+
+
     const elementName=getElementByIdText('trangle')
     const cmSquare=getElementByIdText('tri-cm-square')
     const tringleValueA = getElementById('tri-area-a');
     const tringleValueB = getElementById('tri-area-b');
-    const trangleArea = .5 *( parseFloat(tringleValueA) * parseFloat(tringleValueB));
+    const trangleAreaFixed = .5 *( parseFloat(tringleValueA) * parseFloat(tringleValueB));
+    const trangleArea = trangleAreaFixed.toFixed(2);
     console.log(trangleArea);
 
+    // validation for input value..........
+    if (isNaN(tringleValueA) || isNaN(tringleValueB) ) {
+        alert('Please enter only Number')
+        return;
+    }
+   else if (tringleValueA < 0 || tringleValueB < 0 ) {
+        alert("Provide valid input");
+        return;
+    }
 
     const container=document.getElementById('table-container');
     const tr = document.createElement('tr');
@@ -20,7 +34,6 @@ document.getElementById('trangle-btn').addEventListener('click', function () {
     <td>${elementName}</td>
     <td>${trangleArea}</td>
     <td>${cmSquare}</td>
-    
     `
     container.appendChild(tr);
 })
@@ -32,8 +45,19 @@ document.getElementById('rectangle-btn').addEventListener('click', function () {
     const cmSquare=getElementByIdText('rec-cm-square');
     const recValueA = getElementById('rec-area-a');
     const recValueB = getElementById('rec-area-b');
-    const rectangleArea = parseFloat(recValueA) * parseFloat(recValueB);
+    const rectangleAreaFixed = parseFloat(recValueA) * parseFloat(recValueB);
+    const rectangleArea =rectangleAreaFixed.toFixed(2) ;
     console.log(rectangleArea);
+
+    if (isNaN(recValueA) || isNaN(recValueB) ) {
+        alert('Please enter only Number')
+        return;
+    }
+   else if (recValueA < 0 || recValueB < 0 ) {
+        alert("Provide valid input");
+        return;
+    }
+
 
     const container=document.getElementById('table-container');
     const tr = document.createElement('tr');
@@ -56,8 +80,8 @@ document.getElementById('peralleogram-btn').addEventListener('click', function (
     const cmSquare=getElementByIdText('peralleo-cm-square');
     const peralleoValueA = getElementById('peralleo-area-a');
     const peralleoValueB = getElementById('peralleo-area-b');
-    const peralleoArea = parseFloat(peralleoValueA) * parseFloat(peralleoValueB);
-    console.log(peralleoArea);
+    const peralleoAreaFixed = parseFloat(peralleoValueA) * parseFloat(peralleoValueB);
+    const peralleoArea = peralleoAreaFixed.toFixed(2);
 
     const container=document.getElementById('table-container');
     const tr = document.createElement('tr');
@@ -73,27 +97,64 @@ document.getElementById('peralleogram-btn').addEventListener('click', function (
 // four card
 document.getElementById('rohumbos-btn').addEventListener('click', function () {
 
+
+    const elementName=getElementByIdText('rohumbos');
+    const cmSquare=getElementByIdText('rohumbos-cm-square');
     const peralleoValueA = getElementById('rombos-area-a');
     const peralleoValueB = getElementById('rombos-area-b');
-    const rohumbosArea = 0.5 * parseFloat(peralleoValueA) * parseFloat(peralleoValueB);
-    console.log(rohumbosArea);
+    const rohumbosAreaFixed = 0.5 * parseFloat(peralleoValueA) * parseFloat(peralleoValueB);
+    const rohumbosArea =rohumbosAreaFixed.toFixed(2);
+    
+    const container=document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML=`
+    <td>${1}</td>
+    <td>${elementName}</td>
+    <td>${rohumbosArea}</td>
+    <td>${cmSquare}</td>
+    `
+    container.appendChild(tr); 
 })
 
 // fifth card
 document.getElementById('pentagon-btn').addEventListener('click', function () {
 
+
+    const elementName=getElementByIdText('pentagon');
+    const cmSquare=getElementByIdText('pentagon-cm-sqpare');
     const pentagonValueA = getElementById('pentagon-area-a');
     const pentagonValueB = getElementById('pentagon-area-b');
-    const pentagonArea = 0.5 * parseFloat(pentagonValueA) * parseFloat(pentagonValueB);
-    console.log(pentagonArea);
+    const pentagonAreaFixed = 0.5 * parseFloat(pentagonValueA) * parseFloat(pentagonValueB);
+    const pentagonArea = pentagonAreaFixed.toFixed(2);
+
+    const container=document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML=`
+    <td>${1}</td>
+    <td>${elementName}</td>
+    <td>${pentagonArea}</td>
+    <td>${cmSquare}</td>
+    `
+    container.appendChild(tr);
 })
 
 // sixth card
 document.getElementById('ellips-btn').addEventListener('click', function () {
 
+    const elementName=getElementByIdText('ellips');
+    const cmSquare=getElementByIdText('ellips-cm-sqpare');
     const ellipsValueA = getElementById('ellips-area-a');
     const ellipsValueB = getElementById('ellips-area-b');
-    const ellipsArea = 0.14 * parseFloat(ellipsValueA) * parseFloat(ellipsValueB);
+    const ellipsArea = 3.1416 * parseFloat(ellipsValueA) * parseFloat(ellipsValueB);
     const ellipsAreaToFixed = ellipsArea.toFixed(2);
-    console.log(ellipsAreaToFixed);
+
+    const container=document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML=`
+    <td>${1}</td>
+    <td>${elementName}</td>
+    <td>${ellipsAreaToFixed}</td>
+    <td>${cmSquare}</td>
+    `
+    container.appendChild(tr);
 })
--- utilities.js
+++ utilities.js
@@ -1,11 +1,12 @@
 
+// This function use for the input field by there id ............
 function getElementById(inputId) {
     const areaValueOfA = document.getElementById(inputId);
     const areaNumber = areaValueOfA.value;
     return areaNumber;
 }
 
-
+// This function use foe the text field by there id...............
 function getElementByIdText(inputIdtext) {
     const areaValueOfA = document.getElementById(inputIdtext);
     const areaNumber = areaValueOfA.innerText;
